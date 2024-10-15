bb6c94a0498964f7fbcb6a4ef4050e07e102c143
dangductuong <tuongdd.24itb@vku.udn.vn>
2024-10-16 01:09:31 +0700

    baitap

---
+++ 1.2/src/Circle.java
@@ -0,0 +1,21 @@
+public class Circle {
+    private double radius;
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+    public double getCirumference() {
+        return 2 * Math.PI * radius;
+    }
+    public String toString() {
+        return radius + " " + getArea() + " " + getCirumference();
+    }
+}
