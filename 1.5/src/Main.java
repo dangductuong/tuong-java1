c3247ae2bcd8333c3a2cb1fb30fa25bbdff8a899
dangductuong <tuongdd.24itb@vku.udn.vn>
2024-10-16 08:24:34 +0700

    baitap

---
+++ 1.5/src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        InvoiceItem invoiceItem = new InvoiceItem("A101","Pen Red",999,0.99);
+        System.out.println(invoiceItem.toString());
+
+    }
+}
